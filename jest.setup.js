a77b44d3e5adfe61f308053ad5fbf8a6b3613ffb
cotoaga <kurt@cotoaga.de>
2025-07-13 13:26:17 +0200

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,6 @@
-import '@testing-library/jest-dom'
+// jest.setup.js - Use require instead of import
+require('@testing-library/jest-dom')
+
 global.Response = class Response {
   static json(data) {
     return { json: () => Promise.resolve(data) }
